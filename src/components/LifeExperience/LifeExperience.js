5cbd298d14431214cc67d8dee05a887df0ac7ede
Ramsey lewis <ramseylewis99@gmail.com>
2020-11-30 21:29:26 -0500

    text for life ex inputed

--- src/components/LifeExperience/LifeExperience.js
+++ src/components/LifeExperience/LifeExperience.js
@@ -13,16 +13,22 @@ class LifeExperience extends Component{
             <div className = 'LifeExperience' name='LifeExperience'>
                 <div className='LifeExperienceTitle'>  < h2> Philanthropy </h2> </div>
                 <div className='LifeExperienceCard'>
-                    <p> 'Gastropub tofu la croix adaptogen salvia literally taiyaki. Flexitarian tbh vaporware man bun listicle franzen unicorn squid deep v normcore locavore godard. Roof party organic messenger bag, knausgaard jean shorts sriracha letterpress fanny pack cred disrupt raclette truffaut.' </p>
-                    <p> 'Gastropub tofu la croix adaptogen salvia literally taiyaki. Flexitarian tbh vaporware man bun listicle franzen unicorn squid deep v normcore locavore godard. Roof party organic messenger bag, knausgaard jean shorts sriracha letterpress fanny pack cred disrupt raclette truffaut.' </p>
-                    <p> 'Gastropub tofu la croix adaptogen salvia literally taiyaki. Flexitarian tbh vaporware man bun listicle franzen unicorn squid deep v normcore locavore godard. Roof party organic messenger bag, knausgaard jean shorts sriracha letterpress fanny pack cred disrupt raclette truffaut.' </p>
-                    <p> 'Gastropub tofu la croix adaptogen salvia literally taiyaki. Flexitarian tbh vaporware man bun listicle franzen unicorn squid deep v normcore locavore godard. Roof party organic messenger bag, knausgaard jean shorts sriracha letterpress fanny pack cred disrupt raclette truffaut.' </p>
+                    <p> C.O.T.A - COUNCIL OF THOUGHT AND ACTION  </p>
+                    <p> Organizer of projects & events like job fairs. 
+                        Managed a summer camp for youth while also helping parents & adults find work. 
+                        The objective of C.O.T.A is reducing recidivism one person at a time and improving individual lives, the community and the World as a whole.
+                        My involvment in the C.O.T.A movemnt has spanded most of life, 
+                        the organization was actually started by my mother in the early 2000's deep into my childhood. 
+                        An emphasis on empathy and community was intilled in me from these experiences, aswell as an ungency to get the job done on time.
+                        My actions effect the people around me just like everyone else, if more people felt this way the world would be a better place.
+                    </p>
+
                 </div>
-                <div className='LifeExperienceTitle'>  < h2> Modeling </h2> </div>
+                <div className='LifeExperienceTitle'>  < h2> Business In Fashion </h2> </div>
                 <div className='LifeExperienceCard'>
                     <p> 'Gastropub tofu la croix adaptogen salvia literally taiyaki. Flexitarian tbh vaporware man bun listicle franzen unicorn squid deep v normcore locavore godard. Roof party organic messenger bag, knausgaard jean shorts sriracha letterpress fanny pack cred disrupt raclette truffaut.' </p>
-                    <p> 'Gastropub tofu la croix adaptogen salvia literally taiyaki. Flexitarian tbh vaporware man bun listicle franzen unicorn squid deep v normcore locavore godard. Roof party organic messenger bag, knausgaard jean shorts sriracha letterpress fanny pack cred disrupt raclette truffaut.' </p>
-                    <p> 'Gastropub tofu la croix adaptogen salvia literally taiyaki. Flexitarian tbh vaporware man bun listicle franzen unicorn squid deep v normcore locavore godard. Roof party organic messenger bag, knausgaard jean shorts sriracha letterpress fanny pack cred disrupt raclette truffaut.' </p>
+                     'Gastropub tofu la croix adaptogen salvia literally taiyaki. Flexitarian tbh vaporware man bun listicle franzen unicorn squid deep v normcore locavore godard. Roof party organic messenger bag, knausgaard jean shorts sriracha letterpress fanny pack cred disrupt raclette truffaut.'
+                    'Gastropub tofu la croix adaptogen salvia literally taiyaki. Flexitarian tbh vaporware man bun listicle franzen unicorn squid deep v normcore locavore godard. Roof party organic messenger bag, knausgaard jean shorts sriracha letterpress fanny pack cred disrupt raclette truffaut.'
                     <p> 'Gastropub tofu la croix adaptogen salvia literally taiyaki. Flexitarian tbh vaporware man bun listicle franzen unicorn squid deep v normcore locavore godard. Roof party organic messenger bag, knausgaard jean shorts sriracha letterpress fanny pack cred disrupt raclette truffaut.' </p>
                 </div>
             </div> 

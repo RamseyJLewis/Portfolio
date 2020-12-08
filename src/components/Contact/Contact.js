a1e9c16ee3ba9c4cac88b9748801d0aa69eb1529
Ramsey lewis <ramseylewis99@gmail.com>
2020-12-08 12:30:21 -0500

    contact area on page has svg

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,38 +1,33 @@
 import React, { Component } from 'react';
-import github from '../../Assets/github.png'
-import phone from '../../Assets/phone.png'
-import linkedin from '../../Assets/linkedin.png'
 import './Contact.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {faGithub, faLinkedin} from '@fortawesome/free-brands-svg-icons'
-class Contact extends Component{
-    constructor(props){
+import { faGithub, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+
+class Contact extends Component {
+    constructor(props) {
         super(props);
-        this.state ={};
+        this.state = {};
     }
 
-
-    render(){
-        return(
-            <div className = 'Contact' name='Contact'>
+    render() {
+        return (
+            <div className='Contact' name='Contact'>
                 <div className='ContactTitle'>  < h2> Get In Touch </h2> </div>
                 <div className='ContactCard'>
-                    <p> 631-639-4366</p>
-                    <p> TheRamseyLewis@gmail.com</p>
-                    <div className='logo'>
-                        {/* <img src={github} alt="Logo" />
-                        <img src={phone} alt="Logo" />
-                        <img src={linkedin} alt="Logo" />   */}
-
-                        <FontAwesomeIcon icon= {faGithub} />
-                        <FontAwesomeIcon icon= {faLinkedin} />
-
+                    <div className='contactText'>
+                        <p> (631-639-4366) </p>
+                        <p>  TheRamseyLewis@gmail.com</p>
+                    </div>
+                    <div className='logo'>                       
+                        <FontAwesomeIcon icon={faGithub} />
+                        <FontAwesomeIcon icon={faLinkedin} />
+                        <FontAwesomeIcon icon={faInstagram} />
                     </div>
                 </div>
-            </div> 
-            )
+            </div>
+        )
     }
 }
 
 
- export default Contact
+export default Contact

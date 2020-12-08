2cc6612550c4cfd16f6f300a4b0271a33af4fdcc
Ramsey lewis <ramseylewis99@gmail.com>
2020-12-08 17:47:44 -0500

    life ex area sizes properly

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -6,7 +6,7 @@ const LandingPage = () =>{
         <p className='Greeting'>Howdy, my name is </p>
         <h1 className= 'Name'>Ramsey Lewis</h1>
         <h2 className='MainText'> & I'm a Software Engineer.</h2>
-        <p className='Objective'>I am a firm believer that hard work and ambition are contagious. My primary goal is to impact the world in a positive way through these principles.</p>
+        <p className='Objective'>I am a 21 year old software engineer based in Ny. Im currently looking for employment in software development local in Ny or Remote.   </p>
         
     </div>
     )

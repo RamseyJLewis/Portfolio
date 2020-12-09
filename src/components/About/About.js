34ac075379c6c54027b6fac4f35ecb72c8dc6e0c
Ramsey lewis <ramseylewis99@gmail.com>
2020-12-08 19:08:30 -0500

    About text done

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -12,10 +12,12 @@ class About extends Component{
             <div className = 'About' name='About'>
                 <div className='AboutTitle'>  < h2> About   Me </h2> </div>
                 <div className='AboutCard'>
-                    <p> 'Gastropub tofu la croix adaptogen salvia literally taiyaki. Flexitarian tbh vaporware man bun listicle franzen unicorn squid deep v normcore locavore godard. Roof party organic messenger bag, knausgaard jean shorts sriracha letterpress fanny pack cred disrupt raclette truffaut.' </p>
-                    <p> 'Gastropub tofu la croix adaptogen salvia literally taiyaki. Flexitarian tbh vaporware man bun listicle franzen unicorn squid deep v normcore locavore godard. Roof party organic messenger bag, knausgaard jean shorts sriracha letterpress fanny pack cred disrupt raclette truffaut.' </p>
-                    <p> 'Gastropub tofu la croix adaptogen salvia literally taiyaki. Flexitarian tbh vaporware man bun listicle franzen unicorn squid deep v normcore locavore godard. Roof party organic messenger bag, knausgaard jean shorts sriracha letterpress fanny pack cred disrupt raclette truffaut.' </p>
-                    <p> 'Gastropub tofu la croix adaptogen salvia literally taiyaki. Flexitarian tbh vaporware man bun listicle franzen unicorn squid deep v normcore locavore godard. Roof party organic messenger bag, knausgaard jean shorts sriracha letterpress fanny pack cred disrupt raclette truffaut.' </p>
+                    <p> I'm Ramsey, a software engineer based in NY.</p>
+                    <p> I love technology, humanity, creativity and solving problems. I am looking for a company to give my skills to. Whatever computer language I would have to learn to help a company be open to, I want to grow as a software engineer. I went to the Fashion Institute of Technology for a few years and ended up working in the Fashion Industry for a few years as a creative all while still having an interest in fashion. I then graduated from my software engineering bootcamp Nebula Academy on January 1st 2021. </p>
+                    <p> I've listed a few  technologies I've been working with  recently below: </p>
+                    <p> JavaScript, React, HTML, CSS,</p>
+                    <p>Java, JSX, PostgreSQL, AWS S3</p>
+                    <p> AWS EC2, AWS Cloudfront, AWS IAM, Node.js </p>
                 </div>
             </div> 
             )
--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -6,7 +6,8 @@ const LandingPage = () =>{
         <p className='Greeting'>Howdy, my name is </p>
         <h1 className= 'Name'>Ramsey Lewis</h1>
         <h2 className='MainText'> & I'm a Software Engineer.</h2>
-        <p className='Objective'>I am a 21 year old software engineer based in Ny. Im currently looking for employment in software development local in Ny or Remote.   </p>
+        <p className='Objective'>
+I am a 21 year old software engineer based in NY. I'm currently looking for employment in software development in NY or Remote.</p>
         
     </div>
     )

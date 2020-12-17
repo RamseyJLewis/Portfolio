b2afd2b3a9bb9ea2963527b0a3c96a4bd63659a4
Ramsey lewis <ramseylewis99@gmail.com>
2020-12-16 22:01:37 -0500

    info about me p tag edit

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -13,7 +13,7 @@ class About extends Component{
                 <div className='AboutTitle'>  < h2> About   Me </h2> </div>
                 <div className='AboutCard'>
                     <p> I'm Ramsey, a software engineer based in NY.</p>
-                    <p> I love technology, humanity, creativity and solving problems. I am looking for a company to give my skills to. Whatever computer language I would have to learn to help a company be open to, I want to grow as a software engineer. I went to the Fashion Institute of Technology for a few years and ended up working in the Fashion Industry for a few years as a creative all while still having an interest in fashion. I then graduated from my software engineering bootcamp Nebula Academy on January 1st 2021. </p>
+                    <p> I love technology, humanity, creativity and solving problems. I am looking for a company to give my skills to. Whatever  additional computer languages would be required for me to acquire, I am ready, willing and able to do just that.  I want to grow as a software engineer. I attended the Fashion Institute of Technology  and got the opportunity to work in the Fashion Industry for a few years on the creative side of the industry,  I then  found  the niche of software engineering where I can take a creative approach utilizing data resources. I have now graduated from my software engineering bootcamp Nebula Academy on January 1st 2021 and I am ready to pull all of my experience and training to the mission of a company.</p>
                     <p> I've listed a few  technologies I've been working with  recently below: </p>
                     <p> JavaScript, React, HTML, CSS,</p>
                     <p>Java, JSX, PostgreSQL, AWS S3</p>
--- src/components/LifeExperience/LifeExperience.js
+++ src/components/LifeExperience/LifeExperience.js
@@ -14,13 +14,14 @@ class LifeExperience extends Component{
                 <div className='LifeExperienceTitle'>  < h2> Philanthropy </h2> </div>
                 <div className='LifeExperienceCard'>
                     <p> C.O.T.A - COUNCIL OF THOUGHT AND ACTION  </p>
-                    <p> Organizer of projects & events such as job fairs. Managed a summer camp for youth while also helping parents & adults find work. The objective of C.O.T.A. is to increase employment rates among formerly incarcerated individuals while reducing re-incarceration rates, improving individual lives and communities.
+                    <p> Organized projects  and events such as job fairs and ran summer classes for youth. This led me to co-manage a summer camp for youth while also helping parents & adults find work. The objective of C.O.T.A. is to increase employment rates among the  formerly incarcerated while reducing re-incarceration rates, improving individual’s lives and developing opportunities for at-risk youth.
+
                     </p>
 
                 </div>
                 <div className='LifeExperienceTitle'>  < h2> Business In Fashion </h2> </div>
                 <div className='LifeExperienceCard'>
-                    <p> My modeling career got its start at the Fashion Institute of Technology in 2019. Since then I've worked on countless photoshoots, been in multiple magazines and walked on runways. One of my main takeaways from modeling is how important the work environment is,  collaboration is a key skill to have in every industry. Often how well or poorly a team works is how well or poorly the final product comes out. My second takeaway from modeling is how important time management is, wasting people's  time is the fastest way to create problems </p>
+                    <p> My modeling career got its start at the Fashion Institute of Technology in 2019. Since then I've worked on countless photoshoots, been in multiple magazines and walked on runways. One of my main takeaways from modeling is the importance of the work environment. Cooperation,   </p>
                 </div>
             </div> 
             )

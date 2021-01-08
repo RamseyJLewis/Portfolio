b1730cbfde0ef48a1cadedb0fe0a4c1ea434371d
Ramsey lewis <ramseylewis99@gmail.com>
2021-01-07 20:07:21 -0500

    took out age

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -3,11 +3,11 @@ import './LandingPage.css'
 const LandingPage = () =>{
     return (
     <div className='LandingPage'>
-        <p className='Greeting'>Howdy, my name is </p>
+        <p className='Greeting'>Hi, my name is </p>
         <h1 className= 'Name'>Ramsey Lewis</h1>
         <h2 className='MainText'> & I'm a Software Engineer.</h2>
         <p className='Objective'>
-I am a 21 year old software engineer based in NY. I'm currently looking for employment in software development in NY or Remote.</p>
+I am a software engineer based in NY. I'm currently looking for employment in software development in NY or Remote.</p>
         
     </div>
     )

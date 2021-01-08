cf862b30d67c3b6d87bbd84a739a2c6e13e22700
Ramsey lewis <ramseylewis99@gmail.com>
2021-01-08 13:46:35 -0500

    text editeds

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -11,8 +11,7 @@ class About extends Component{
         return(
             <div className = 'About' name='About'>
                 <div className='AboutTitle'>  < h2> About   Me </h2> </div>
-                <div className='AboutCard'>
-                    <p> I'm Ramsey, a software engineer based in NY.</p>
+                <div className='AboutCard'>                   
                     <p> I love technology, humanity, creativity and solving problems. I am looking for a company to give my skills to. Whatever  additional computer languages would be required for me to acquire, I am ready, willing and able to do just that.  I want to grow as a software engineer. I attended the Fashion Institute of Technology  and got the opportunity to work in the Fashion Industry for a few years on the creative side of the industry,  I then  found  the niche of software engineering where I can take a creative approach utilizing data resources. I have now graduated from my software engineering bootcamp Nebula Academy on January 1st 2021 and I am ready to pull all of my experience and training to the mission of a company.</p>
                     <p> I've listed a few  technologies I've been working with  recently below: </p>
                     <p> JavaScript, React, HTML, CSS,</p>
--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -4,8 +4,8 @@ const LandingPage = () =>{
     return (
     <div className='LandingPage'>
         <p className='Greeting'>Hi, my name is </p>
-        <h1 className= 'Name'>Ramsey Lewis</h1>
-        <h2 className='MainText'> & I'm a Software Engineer.</h2>
+        <h1 className= 'Name'>Ramsey Lewis,</h1>
+        <h2 className='MainText'> & I'm a builder.</h2>
         <p className='Objective'>
 I am a software engineer based in NY. I'm currently looking for employment in software development in NY or Remote.</p>
         

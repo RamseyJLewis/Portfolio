91e542f87b5a30bb6c163c149ef006599a345d73
Ramsey lewis <ramseylewis99@gmail.com>
2021-01-17 22:03:46 -0500

    wwd photo in portfolio page

--- src/components/LifeExperience/LifeExperience.js
+++ src/components/LifeExperience/LifeExperience.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+import WWD from '../../Assets/WWD.RH.png'
 import './LifeExperince.css';
+
 
 class LifeExperience extends Component{
     constructor(props){
@@ -17,9 +19,9 @@ class LifeExperience extends Component{
                     <p> Organized projects  and events such as job fairs and ran summer classes for youth. This led me to co-manage a summer camp for youth while also helping parents & adults find work. The objective of C.O.T.A. is to increase employment rates among the  formerly incarcerated while reducing re-incarceration rates, improving individual’s lives and developing opportunities for at-risk youth.
 
                     </p>
-
                 </div>
                 <div className='LifeExperienceTitle'>  < h2> Business In Fashion </h2> </div>
+                <div> <img  src={WWD} alt='WWD'/> </div>
                 <div className='LifeExperienceCard'>
                     <p>  My modeling career got its start at the Fashion Institute of Technology in 2019. Since then I've worked on countless photoshoots, been in multiple magazines and walked on runways. One of my main takeaways from modeling is the importance of the work environment. Cooperation,  collaboration and decorum are the key skills to have in every industry. Often how well or poorly a team works is how well or poorly the final product comes out. My second takeaway from modeling is the importance of time management; wasting people's  time is the fastest way to create problems and undermine the final project.  </p>
                 </div>

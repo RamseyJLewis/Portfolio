abb4cf9904f874cf7bb2d260611fff9a1bb29dbb
Ramsey Lewis <ramseylewis99@gmail.com>
2022-05-19 04:18:19 -0400

    color changes

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -25,7 +25,7 @@ class Portfolio extends Component {
         url: "www.dentsu.com",
         title: "Dentsu",
         desc: "Dentsu Inc. is a Japanese international advertising and public relations joint stock company headquartered in Tokyo. Dentsu is currently the largest advertising agency and the fifth largest advertising agency network in the world in terms of worldwide revenues.",
-        tech: ["JavaScript (ES6)", "CSS", "Adobe Target", "Bit-Bucket", "Jira"],
+        tech: ["JavaScript (ES6)", "CSS", "Adobe Target", "BitBucket", "Jira"],
         img: dentsu,
       },
       {
@@ -49,15 +49,21 @@ class Portfolio extends Component {
         tech: ["HTML", "CSS", "JavaScript (ES6)", "API", "React.js", "ASP.NET"],
         img: Energime,
       },
+      // {
+      //   url: "https://www.unlockthebox.io/",
+      //   title: "Unlock The Box",
+      //   desc: "An interactive web application to assist in teaching students through team based game play. Teachers can create accounts attached to an id connected to the database PostgreSQL, to create, delete , read and update questions for students hosted in rooms.",
+      //   tech: ["React J.S", "JavaScript (ES6)" ,"HTML" ,"CSS" ,"AWS","PostgreSQL"],
+      //   img: card0,
+      // },
       {
-        url: "https://www.unlockthebox.io/",
-        title: "Unlock The Box",
-        desc: "An interactive web application to assist in teaching students through team based game play. Teachers can create accounts attached to an id connected to the database PostgreSQL, to create, delete , read and update questions for students hosted in rooms.",
-        tech: ["React J.S", "JavaScript (ES6)" ,"HTML" ,"CSS" ,"AWS","PostgreSQL"],
-        img: card0,
-      },
-     
-     
+        url: "https://ramseyjlewis.github.io/Space-Force-moon-of-gold/",
+        title: "Space Force Moon of gold",
+        desc: "An arcade shooter inspired by Space Invaders but with 360-degree gameplay. Escape the massive swarm of churning aliens, all of which are being tracked through mathematics, objects and arrays. This object-oriented program is built using JavaScript, HTML and CSS. The Swarm of enemies try to crash into the player. The objective is to last until backup arrives",
+        tech: ["Vanilla-JavaScript", "HTML" ,"CSS"],
+        img: card2,
+      }, 
+
       {
         url: "https://ramseyjlewis.github.io/Not-Flix/",
         title: "Not-Flix",
@@ -65,13 +71,7 @@ class Portfolio extends Component {
         tech: [ "Vanilla-JavaScript","HTML" ,"CSS" ,"API"],
         img: card1,
       },
-      {
-        url: "https://ramseyjlewis.github.io/Space-Force-moon-of-gold/",
-        title: "Space Force Moon of gold",
-        desc: "An arcade shooter inspired by Space Invaders but with 360-degree gameplay. Escape the massive swarm of churning aliens, all of which are being tracked through mathematics, objects and arrays. This object-oriented program is built using JavaScript, HTML and CSS. The Swarm of enemies try to crash into the player. The objective is to last until backup arrives",
-        tech: ["Vanilla-JavaScript", "HTML" ,"CSS"],
-        img: card2,
-      },     
+     
     ];
 
     return (

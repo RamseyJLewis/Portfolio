6b02db723f79945a34296d829904949d8eed620f
Ramsey lewis <ramseylewis99@gmail.com>
2022-07-05 01:06:41 -0400

    bug fix test

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -14,7 +14,7 @@ const LandingPage = () => {
           <Fade right><div className='headshotHov'><img className="headShot" src={headshot} alt="headshot" />{" "}</div></Fade> 
         </h1>
       </div>
-      <Fade left>
+      <Fade left cascade>
         {" "}
         <p className="Greeting">
               {" "}

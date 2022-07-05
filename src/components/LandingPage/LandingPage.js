29193987e817f29134063749a0b9e91d072c36d7
Ramsey lewis <ramseylewis99@gmail.com>
2022-07-05 01:12:16 -0400

    npm update

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -14,7 +14,7 @@ const LandingPage = () => {
           <Fade right><div className='headshotHov'><img className="headShot" src={headshot} alt="headshot" />{" "}</div></Fade> 
         </h1>
       </div>
-      <Fade left cascade>
+      <Fade left >
         {" "}
         <p className="Greeting">
               {" "}
